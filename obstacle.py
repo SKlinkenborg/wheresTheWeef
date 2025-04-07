c3be1d790e1efde96f48c152445a4916254cbff1
brucef0rte <bfrte71@gmail.com>
2025-04-06 19:17:55 -0500

    add obstacle file

---
+++ obstacle.py
@@ -0,0 +1,5 @@
+# Using the same animation technique from animation.py
+# Create a list of frame numbers from frames list
+# If player input is received within correct frame # range
+# Player successfully passes obstacle
+# If not: -2 Stamina & try again
